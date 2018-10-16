20da6ece228e9e2d7974ebbdd2373c6efcc25828
David Amos <somacdivad@gmail.com>
2018-10-16 01:58:16 -0500

    Add Orbit class

---
+++ pyglyt/group/orbit.py
@@ -0,0 +1,42 @@
+import itertools
+import functools
+from typing import Any, Callable
+
+from .group import Group
+
+
+class Orbit:
+    """Orbit class"""
+
+    def __init__(
+        self, group: Group, element: Any, action: Callable, cache: bool = True
+    ) -> None:
+        self.group = group
+        self.element = element
+        self.action = action
+        self._cache = cache
+        if cache:
+            self._elements = []
+
+    def __iter__(self):
+        """Return an iterator over element of the orbit.
+
+        Warning: This may cause an infinite loop for infinite groups.
+        """
+        if self._cache and self._elements:
+            for element in self._elements:
+                yield element
+        else:
+            self._elements.append(self.element)
+            yield self.element
+            _powers = self.group._powers
+            _generators = self.group.generators
+            # Below line blows up for infinite groups
+            powers_of_generators = tuple(_powers(g) for g in _generators)
+            for g in itertools.product(*powers_of_generators):
+                x = functools.reduce(self.group.operation, g)
+                y = self.action(self.element, x)
+                if y not in self._elements:
+                    if self._cache:
+                        self._elements.append(y)
+                    yield y
